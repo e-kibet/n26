1aadf2abbbde91a934672865a941697d1e19176f
Ephas Khajira <fasoh@Ephass-MacBook-Pro.local>
2021-03-26 07:44:14 +0300

    code analysis run

--- domain/src/main/java/com/n26/domain/dbrepository/StatsRepository.kt
+++ domain/src/main/java/com/n26/domain/dbrepository/StatsRepository.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 N26
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.n26.domain.dbrepository
 
 import com.n26.domain.model.StatsDomainModel
@@ -5,6 +20,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface StatsRepository {
 
-    //TODO Maybe return a page. I dont know just a thought
+    // TODO Maybe return a page. I dont know just a thought
     fun getStats(): Flow<List<StatsDomainModel>>
 }
