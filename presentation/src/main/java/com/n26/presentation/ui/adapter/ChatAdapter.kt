14b1bfafd6606e5fd1204aa2b73ea4a94c0d506a
Harun Wangereka <harunwangereka@gmail.com>
2021-03-26 16:16:15 +0300

    Added Chart Data

--- presentation/src/main/java/com/n26/presentation/ui/adapter/ChatAdapter.kt
+++ presentation/src/main/java/com/n26/presentation/ui/adapter/ChatAdapter.kt
@@ -1,4 +1,14 @@
 package com.n26.presentation.ui.adapter
 
-class ChatAdapter {
+import com.robinhood.spark.SparkAdapter
+
+class ChatAdapter: SparkAdapter() {
+    private val testData = listOf<Float>(
+        1.11f,2.11f
+    )
+    override fun getCount(): Int = testData.size
+
+    override fun getItem(index: Int): Float  = testData[index]
+
+    override fun getY(index: Int): Float = testData[index]
 }
--- presentation/src/main/java/com/n26/presentation/ui/views/MainActivity.kt
+++ presentation/src/main/java/com/n26/presentation/ui/views/MainActivity.kt
@@ -5,17 +5,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.n26.presentation.R
+import com.n26.presentation.databinding.ActivityMainBinding
+import com.n26.presentation.ui.adapter.ChatAdapter
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setupChat()
+    }
+
+    private fun setupChat() {
+        binding?.sparkview?.adapter = ChatAdapter()
+
     }
 }
