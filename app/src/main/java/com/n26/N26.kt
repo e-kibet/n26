d16665ebdcb1fa406b90817f6ca13327f023e2b5
Juma Allan <allanjuma@gmail.com>
2021-03-26 22:37:59 +0300

    chore: updated application class

--- app/src/main/java/com/n26/N26.kt
+++ app/src/main/java/com/n26/N26.kt
@@ -16,10 +16,55 @@
 package com.n26
 
 import android.app.Application
+import androidx.annotation.Nullable
+import com.example.shared.utils.CrashlyticsTree
+import com.n26.database.di.dataModule
+import com.n26.domain.di.domainModules
+import com.n26.network.di.networkModule
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import org.koin.core.logger.Level
+import org.koin.core.module.Module
+import timber.log.Timber
 
 class N26 : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        initKoin()
+        initTimber()
+    }
+
+    private fun initKoin() {
+        try {
+            startKoin {
+                androidLogger(Level.ERROR)
+                androidContext(applicationContext)
+                val modules = mutableListOf<Module>().apply {
+                    addAll(domainModules)
+                    addAll(networkModule)
+                    addAll(dataModule)
+                }
+                modules(modules)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
+    }
+
+    private fun initTimber() = when {
+        BuildConfig.DEBUG -> {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+        else -> Timber.plant(CrashlyticsTree())
     }
 }
